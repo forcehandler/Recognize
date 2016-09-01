ee5aa1112bb8f1e28dbf1c6fb6035984e411531c
Aaditya Vaze <f2014603@goa.bits-pilani.ac.in>
2016-09-02 00:35:01 +0530

    android app initialized

---
+++ app/Recognize/app/src/main/java/ai/bits/recognize/Constants.java
@@ -0,0 +1,12 @@
+package ai.bits.recognize;
+
+/**
+ * Created by Vaze on 9/2/2016.
+ */
+public class Constants {
+    public interface Music
+    {
+        String SONG_STARTED_URL="";
+
+    }
+}
---
+++ app/Recognize/app/src/main/java/ai/bits/recognize/MainActivity.java
@@ -0,0 +1,13 @@
+package ai.bits.recognize;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
